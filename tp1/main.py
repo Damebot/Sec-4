c948c366e6450f70a2e08d14b8ae3d996b78e259
Damebot <79015202+Damebot@users.noreply.github.com>
2023-09-19 12:08:11 -0400

    main.py

--- tp1/main.py
+++ tp1/main.py
@@ -4,7 +4,5 @@
 
 #definir le conteur de mots
 def count_word(input_string):
-    #input pour le savoir le nombre de mots
     final_count = len(input_string.split())
-    #dit le nombre de mots
-    print(final_count)
+    return(final_count)
