1bd19c81d715d79aca074e7139588122e771f614
Damebot <79015202+Damebot@users.noreply.github.com>
2024-01-26 11:19:34 -0500

    main.py

--- tp4/main.py
+++ tp4/main.py
@@ -2,12 +2,15 @@
 #31 aout 2023
 #TP4
 
+#imports
 import arcade
 import random
 
+#taille de l'ecran
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
+#fonctions + comment la balle agit
 class Balle:
     def __init__(self, x, y):
         self.x = x
@@ -17,6 +20,7 @@ class Balle:
         self.rayon = random.randint(10, 30)
         self.color = arcade.color.BLUE  # Changer la couleur si nécessaire
 
+    #fonctions de rebondissement
     def update(self):
         self.x += self.change_x
         self.y += self.change_y
@@ -27,9 +31,11 @@ class Balle:
         if self.y < 0 or self.y > SCREEN_HEIGHT:
             self.change_y *= -1
 
+    #dessiner sur l'ecran
     def draw(self):
         arcade.draw_circle_filled(self.x, self.y, self.rayon, self.color)
 
+#comment le rectangle agit + fonctions
 class Rectangle:
     def __init__(self, x, y):
         self.x = x
@@ -41,6 +47,7 @@ class Rectangle:
         self.color = arcade.color.RED  # Changer la couleur si nécessaire
         self.angle = 0
 
+    #fonction de rebondissement
     def update(self):
         self.x += self.change_x
         self.y += self.change_y
@@ -51,9 +58,11 @@ class Rectangle:
         if self.y < 0 or self.y > SCREEN_HEIGHT:
             self.change_y *= -1
 
+    #dessiner avec les fonctions mises sur l'ecran
     def draw(self):
         arcade.draw_rectangle_filled(self.x, self.y, self.width, self.height, self.color, self.angle)
 
+#tout dessiner sur l'ecran final avec nos classes
 class MyGame(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
@@ -61,6 +70,7 @@ class MyGame(arcade.Window):
         self.balle_list = []
         self.rectangle_list = []
 
+    #dessiner
     def on_draw(self):
         arcade.start_render()
         for balle in self.balle_list:
@@ -68,6 +78,7 @@ class MyGame(arcade.Window):
         for rectangle in self.rectangle_list:
             rectangle.draw()
 
+    #si on pese sur right click ou left click qu'est ce qu'il arrive
     def on_mouse_press(self, x, y, button, modifiers):
         if button == arcade.MOUSE_BUTTON_LEFT:
             balle = Balle(x, y)
@@ -76,15 +87,18 @@ class MyGame(arcade.Window):
             rectangle = Rectangle(x, y)
             self.rectangle_list.append(rectangle)
 
+    #nombre de FPS et quand le jeux dois update et refresh son ecran
     def update(self, delta_time):
         for balle in self.balle_list:
             balle.update()
         for rectangle in self.rectangle_list:
             rectangle.update()
 
+#set le titre
 def main():
     window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, "Balles et Rectangles")
     arcade.run()
 
+#authentication pour run le program
 if __name__ == "__main__":
     main()
